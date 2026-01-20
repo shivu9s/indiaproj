5a14b6613e4821f92776b3a488a0a474c0a70a13
Shiven Chauhan <153435937+shivu9s@users.noreply.github.com>
2026-01-20 16:59:40 +0530

    Update MainClass.java

--- src/main/java/com/wip/MainClass.java
+++ src/main/java/com/wip/MainClass.java
@@ -1,22 +1,33 @@
 package com.wip;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class MainClass {
-	
-	public int add(int... number) {
-		int result=0;
-		for(int i : number) {
-			result = result +i;
-		}		
-		return result;
-	}
-	
-	public int multiply(int a, int b) {
-		return a*b;
-	}
 
-	public static void main(String[] args) {
-		System.out.println("Java within Kubernetes K*****8 Jenkins ADDED-- Maven Project!!!");
+    @GetMapping("/")
+    public String home() {
+        return "Java + Jenkins + Docker + Kubernetes is RUNNING";
+    }
 
-	}
+    public int add(int... number) {
+        int result = 0;
+        for (int i : number) {
+            result = result + i;
+        }
+        return result;
+    }
 
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainClass.class, args);
+        System.out.println("Spring Boot app started successfully inside Kubernetes");
+    }
 }
